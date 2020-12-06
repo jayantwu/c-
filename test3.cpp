28c5fa1b0bfb50da7718911691cf5645cb8876c1
wujiayang <1064216585@qq.com>
2020-12-06 13:43:43 +0800

    c++

---
+++ test3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+char* getname(void);
+
+int main()
+{
+	
+	char* name;
+	
+	name = getname();
+	
+	cout  << name << " at " << (int*)name << endl;
+	delete [] name;
+	 
+}
+
+char* getname()
+{
+	
+	char temp[80];
+	cout << "Enter your name: ";
+	cin >> temp;
+	char *pn = new char[strlen(temp)+1];
+	strcpy(pn, temp);
+	
+	return pn; 
+	
+}
