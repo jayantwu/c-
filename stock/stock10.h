73322bf2cbebc0da9f536182f09313787d97e8ca
wujiayang <1064216585@qq.com>
2020-12-16 21:47:19 +0800

    c++ 类

---
+++ stock/stock10.h
@@ -0,0 +1,28 @@
+﻿#ifndef STOCK00_H_
+#define STOCK00_H_
+
+#include<string>
+
+class Stock
+{
+private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot()
+	{
+		total_val = shares * share_val;
+	}
+public:
+	Stock();  //默认构造函数
+	Stock(const std::string &co, long n = 0, double pr = 0.0);  //构造函数
+	~Stock(); //析构函数
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show();
+	
+};
+
+#endif
---
+++ stock/usestock2.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+#include "stock10.h"
+int main()
+{
+	using std::cout;
+	cout << "using constructor to create new objects\n";
+	Stock stock1("Nanosmart", 12, 20.0);  //隐式调用构造函数 ，不创建临时对象 
+	stock1.show();
+	Stock stock2 =  Stock("Boffo Objects", 2, 2.0);  //显式调用构造函数， 这种方式有两种方式实现：是否创建一个临时对象 
+	stock2.show();
+	
+	cout << "Assigning stock1 to stock2:\n";
+	stock2 = stock1;  
+	cout << "Listing stock1 and stock2:\n";
+	stock1.show();
+	stock2.show();
+	
+	cout << "using a  constructor to reset an object\n";
+	stock1 = Stock("Nifty Foods", 10, 50.0); //赋值 ，会创建一个临时对象 
+	cout << "revised stock1:\n";
+	stock1.show();
+	cout << "Done!\n"; 
+	
+
+	return 0;
+}
+
+
+/*output
+
+using constructor to create new objects
+Constructor using Nanosmart called
+company: Nanosmart shares: 12
+ share price: $20.000
+total worth: $240.00
+Constructor using Boffo Objects called
+company: Boffo Objects shares: 2
+ share price: $2.000
+total worth: $4.00
+Assigning stock1 to stock2:
+Listing stock1 and stock2:
+company: Nanosmart shares: 12
+ share price: $20.000
+total worth: $240.00
+company: Nanosmart shares: 12
+ share price: $20.000
+total worth: $240.00
+using a  constructor to reset an object
+Constructor using Nifty Foods called
+Bye, Nifty Foods!
+revised stock1:
+company: Nifty Foods shares: 10
+ share price: $50.000
+total worth: $500.00
+Done!
+Bye, Nanosmart!
+Bye, Nifty Foods!
+
+
+*/
