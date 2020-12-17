4aaaa8f8c6abbf2314c87eb673ef8fc5aae52114
wujiayang <1064216585@qq.com>
2020-12-17 22:09:17 +0800

    c++ ADT stack类

---
+++ stack/stack.h
@@ -0,0 +1,19 @@
+﻿#ifndef STACK_H_
+#define STACK_H_
+typedef unsigned long Item;
+
+class Stack
+{
+private:
+	enum {MAX = 10};  //用枚举作为类的符号常量 ,作用域为类 
+	Item items[MAX];
+	int top;
+public:
+	Stack();
+	bool isempty() const;
+	bool isfull() const;
+	bool push(const Item & item);
+	bool pop(Item & item);
+};
+
+#endif
