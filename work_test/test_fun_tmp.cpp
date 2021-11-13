c40d30b18f53abe97123eb9ae306704c12f58b79
“wujiayang” <1064216585@qq.com>
2021-11-13 13:43:30 +0800

    update template using

--- work_test/test_fun_tmp.cpp
+++ work_test/test_fun_tmp.cpp
@@ -5,11 +5,31 @@
 
 using namespace std;
 template<typename T>
-T test1(T & a)
+T fun1(T & a)
 {
     T b;
     cout << a << endl;
     return b;
+}
+template<typename T>
+T* fun2()
+{
+    T* mem = new T();
+    return mem;
+}
+
+
+template<typename T1, typename T2>
+void fun3(T1 x, T2 y)
+{
+    decltype(x+y) z = x + y;
+
+}
+
+template<typename T1, typename T2>
+auto fun4(T1 x, T2 y) -> decltype(x+y)  //trailing return type
+{
+    return x + y;
 }
 
 
@@ -17,5 +37,16 @@ T test1(T & a)
 int main()
 {
     int a = 1;
-    test1(a);
+    fun1(a);
+    int *b = fun2<int>();
+    *b = 5;
+    cout << b << ": " << *b << endl;
+    delete(b);
+
+    fun3(3, 3.3);
+
+    auto z = fun4(3, 3.4);
+    cout << z << endl;
+
+
 }
