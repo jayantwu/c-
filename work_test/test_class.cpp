14ae1ee1562a438a26051d507fb9f7e977e938fe
“wujiayang” <1064216585@qq.com>
2021-12-03 23:14:05 +0800

    [c++]: *this in class ctor

--- work_test/test_class.cpp
+++ work_test/test_class.cpp
@@ -33,6 +33,65 @@ class testA
 
 };
 
+class origin_B {
+    public:
+        origin_B(string n, string d, int b) : 
+            name(n), desc(d), _b(b) {}
+        virtual ~origin_B() = default;
+
+    private:
+        string name;
+        string desc;
+        int _b;
+};
+
+class testB {
+public:
+    testB(string s, int n):a(s), b(n) {}
+    virtual void show() {
+        cout << a << "***" << b << endl;
+    }
+private:
+    string a;
+    int    b;
+};
+//假设场景：testbc 是 依赖与testB的，所以可以以testB作为参数
+//比如： 一个学生，属于一个班级，一个班级是属于一个学校的，这里没有继承关系，但是有依赖关系，包含关系
+//比如，一个国家，省， 市之间的关系
+//比如下面testbc描述了一个市， ctor的参数代表他所属的省
+class testbc {   
+public:
+    testbc(testB & tb) : tb(tb) {};
+    ~testbc()=default;
+    void set(int n) { val = n; cout << "val set to " << n << endl; }
+    void showv() { cout << "the val is " << val << endl; }
+private:
+    testB tb;
+    int val;
+};
+//
+//如果testC看成一个省， 他需要创建很多的市 create（）
+//那么在构造这个省的时候，同时需要创建他所拥有的市 new， 这个时候就用到了 this！
+class testC : public testB {
+public:
+    testC(string s, int n, double d) : testB(s, n), c(d) {} 
+    void show()
+    {
+        testB::show();
+        cout << c << endl;
+    }
+    void create() {
+        ptr_tbc = shared_ptr<testbc> {new testbc { *this }};  // 用testC本身作为参数构造testbc
+    }
+    shared_ptr<testbc> & getptr() {
+        return ptr_tbc;
+    }
+private:
+    double c;
+    shared_ptr<testbc> ptr_tbc;
+};
+
+
 
 int main()
 {
@@ -51,4 +110,11 @@ int main()
     vector<int> v = vector<int>({1, 2, 3});
     for (auto i : v)
         cout << "v=" << i << endl;
+
+    ////////
+    //test class relationship
+    testC tc("mytest", 0, 0.0);
+    tc.create();
+    tc.getptr()->set(5);
+    tc.getptr()->showv();
 }
