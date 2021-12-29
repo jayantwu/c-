d2069ed449243dd940da72762cf4aa2f072336c0
“wujiayang” <1064216585@qq.com>
2021-12-29 15:40:26 +0800

    [c++]: add getbits

--- work_test/test_bit.cpp
+++ work_test/test_bit.cpp
@@ -4,7 +4,7 @@ using namespace std;
 using namespace boost;
 
 
-dynamic_bitset<> writereg(int src, int pos, int size, int v)
+dynamic_bitset<> setbits(int src, int pos, int size, int v)
 {
 
     auto mask = 0;
@@ -25,12 +25,28 @@ dynamic_bitset<> writereg(int src, int pos, int size, int v)
     return dynamic_bitset<>(32, src);
 }
 
+dynamic_bitset<> getbits(int src, int pos, int bsize, int &v)
+{
+    auto mask = 0;
+    while (--bsize >= 0)
+    {
+        mask <<= 1;
+        mask |= 1;
+    }
+    while (--pos >= 0) src >>= 1;
+
+    v = src & mask;
+    return dynamic_bitset<>(32, v);
+}
+
 
 int main()
 {
-    auto ret = writereg(0b11111111111111, 5, 3, 5);
+    auto ret = setbits(0b11111111111111, 5, 3, 5);
     cout << ret << endl;
-
+    int v = 0;
+    auto f2 = getbits(0b11111110011011, 4, 2, v);
+    cout << f2 << endl;
     return 0;
 
 }
