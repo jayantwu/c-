d62235fcc7bad9d5bce8a1981f0c4435c48cb531
“wujiayang” <1064216585@qq.com>
2023-10-26 16:43:33 +0800

    add test for race condition reproduce

--- cppreveiw/test/26test.cpp
+++ cppreveiw/test/26test.cpp
@@ -14,45 +14,46 @@ int num = NUM;
 std::vector<int> v;
 int n = 0;
 int m = 0;
+bool stop = false;
 
 void push()
 {
     while(num--) {
         {
             //scoped_lock lk{mtx};
-            q.push_back(1);
+            q.push_back(m);
         }
+        printf("push succ: %d\n", m);
+        //cout << "push succ: " << m << endl;
         m++;
-        cout << "push succ" << endl;
+        
         std::this_thread::sleep_for(std::chrono::microseconds(4000));
         //std::this_thread::sleep_for(std::chrono::microseconds(40));
     }
     cout << "m: " << m << endl;
 }
 
-void del()
+void pop()
 {
     while(true) {
         if (!q.empty()) {
+            cout << "q.size() " << q.size() << endl;
             //v.push_back(q.front());
+            //cout << "front: " <<  q.front() << endl;
+            printf("front: %d\n", q.front());
             {
                 //scoped_lock lk{mtx};
                 n++;
-                if (q.front() == nullptr) {
-                    cout << "null ptr..." << endl;
-                    break;
-                }
                 q.pop_front();
             }
-            cout << "pop succ" << endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+            //cout << "pop succ" << endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(4));
             //std::this_thread::sleep_for(std::chrono::microseconds(45)); 
         }
-        //if (v.size() >= 1000)
-        // if (n >= 10000)
-        //     break;
 
         cout << "n: " << n << endl;
+        if (stop && q.empty()) 
+            break;        
     }
 }
 
@@ -60,9 +61,12 @@ void del()
 int main()
 {
     thread t1(push);
-    thread t2(del);
+    thread t2(pop);
     t1.join();
+    if (!t1.joinable())
+        stop = true;
     t2.join();
+
     //cout << "v.size() " << v.size() << endl;
     // cout << "m=" << m << endl;
     // cout << "n=" << n << endl;
---
+++ cppreveiw/test/check.py
@@ -0,0 +1,50 @@
+import subprocess
+import sys
+def race_conditon_check(file):
+    l = []
+    d = {"miss": [], "dup": []}
+    with open(file, 'r') as f:
+        for line in f:
+            if "front: " in line:
+                l.append(line[7:-1])
+
+    # check miss 
+    rslt_set = set(l)
+    for i in range(10000):
+        if str(i) not in rslt_set:
+            #print("miss: " + str(i))
+            d["miss"].append(str(i))
+
+
+    # check duplicate
+    s = set()
+    for i in l:
+        if i not in s:
+            s.add(i)
+        else:
+            #print("dup: "+i)
+            d["dup"].append(i)
+    print("check done!")
+    return d
+    
+
+def execute(filename):
+    process = subprocess.Popen(f"./a.out > {filename}", shell=True)
+    process.communicate()
+    print("execute done!")
+
+if __name__ == '__main__':
+    filename = "1.txt"
+    d = {"miss": [], "dup": []}
+    while len(d["miss"]) == 0 and len(d["dup"]) == 0:
+        #execute(filename)
+        d = race_conditon_check(filename)
+    
+    print("miss: ", end='')
+    for i in d["miss"]:
+        print(i, end=' ')
+    print()
+    print("dup: ", end='')
+    for i in d["dup"]:
+        print(i, end=' ')
+    print()
